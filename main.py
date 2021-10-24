82062715d23899ff6d822ba8fc7c0f9271678aee
Tom <22730962+ohitstom@users.noreply.github.com>
2021-10-24 14:37:05 +0100

    main

---
+++ main.py
@@ -0,0 +1,33 @@
+from random import randint
+import requests
+import urllib
+import os
+
+def clipboard(url):
+    command = 'echo ' + url.strip() + '| clip'
+    os.system(command)
+
+def find_game():
+    random_code = randint(10**(10-1), (10**10)-1)
+    url = ("https://www.roblox.com/games/" + str(random_code))
+
+    try:
+        found = urllib.request.urlopen(url).geturl()
+        return found if "Place" not in found else True
+    except:
+        return True
+
+def main():
+    while True:
+        game = find_game()
+        if game != True:
+            os.system('cls')
+            print('[Found Valid URL] -- Added To Your Clipboard\n\n' + game + '\n')
+            clipboard(game)
+            
+            restart = input("Roll Again? Y/N: ")
+            if restart.lower() in ["n", "no"]:
+                break
+
+if __name__ == "__main__":
+    main()
